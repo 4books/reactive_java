1a93446d6e29f991436392b8ad4c8de07d774436
4books <joonwoo9192@gmail.com>
2023-05-15 22:59:52 +0900

    주석 몇개 달았음

--- src/main/java/com/fourbooks/asnyc/AModel.java
+++ src/main/java/com/fourbooks/asnyc/AModel.java
@@ -6,17 +6,23 @@ import lombok.extern.slf4j.Slf4j;
 public class AModel {
 
     public static void main(String[] args) {
+        //동기 blocking
         log.info("Start main");
+        //1. getResult를 호출한다.
         var result = getResult();
+        //3. 결과를 리턴 받는다.
         var nextValue = result + 1;
         assert nextValue == 1;
         log.info("Finish main");
+        //4. 종료
+        //Caller는 Callee의 응답이 올때까지 어떠한 일도 할 수 없다.
 
     }
 
     public static int getResult(){
         log.info("Start getResult");
         try {
+            //2. main 쓰레드는 대기한다.
             Thread.sleep(1000);
         }catch (InterruptedException e){
             throw new RuntimeException(e);
--- src/main/java/com/fourbooks/asnyc/BModel.java
+++ src/main/java/com/fourbooks/asnyc/BModel.java
@@ -9,7 +9,9 @@ import java.util.function.Consumer;
 public class BModel {
 
     public static void main(String[] args) {
+        //비동기 Blocking
         log.info("Start main");
+        //main은 getResult가 결과를 구하고 callback을 실행하기 전까지 아무것도 할 수 없다.
         getResult(integer -> {
             var nextValue = integer + 1;
             assert nextValue == 1;
